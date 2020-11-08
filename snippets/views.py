925efa28262766a64cf6bac001b2c5f75da04734
Ceasar15 <ceasarkwadwo@gmail.com>
2020-11-08 10:19:24 +0000

    added endpoints views and url

--- snippets/views.py
+++ snippets/views.py
@@ -6,10 +6,13 @@ from django.contrib.auth.models import User
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-
-from rest_framework import status, generics
-from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+
+from rest_framework import permissions , status, generics, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
 
 
 # Create your views here.
@@ -75,3 +78,24 @@ class UserDetail(generics.RetrieveAPIView):
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+
+@api_view
+def api_root(request, format=None):
+    return Response(
+        {
+            'users':reverse('user-list', request=request, format=format),
+            'snippets': reverse('snippet-list', request=request, format=format)
+
+        }
+    )
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
