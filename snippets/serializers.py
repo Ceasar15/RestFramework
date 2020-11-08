6a485d95db8eeaf29af744f16374b79f5fe2b492
Ceasar15 <ceasarkwadwo@gmail.com>
2020-11-08 10:33:17 +0000

    relationship and endpoints

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -6,15 +6,19 @@ from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta: 
         model = Snippet
-        fields = ['id','title','code','linenos','language', 'style', 'owner']
-  
-    owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']    
 
 
 class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
     model = User
-    fields = ['id', 'username', 'snippets']
+    fields = ['url', 'id', 'username', 'snippets']
 
 
