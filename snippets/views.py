32408681891e643ec55b8f7111c27af7f47c708c
Ceasar15 <ceasarkwadwo@gmail.com>
2020-11-11 22:53:18 +0000

    few changes to views

--- snippets/views.py
+++ snippets/views.py
@@ -15,69 +15,6 @@ from rest_framework.reverse import reverse
 
 
 
-# Create your views here.
-# permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-# class SnippetList(generics.ListCreateAPIView):
-#     """
-#     List all snippets or create a new one
-#     """
-
-#     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
-     
-
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request, *args, **kwargs)
-
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request, *args, **kwargs)
-
-#     def perform_create(self, serializer):
-#         serializer.save(owner=self.request.user)
-
-
-
-# permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-# class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-#     """
-#     Retrieve, update or delete a snippet instance.
-#     """
-    
-#     queryset = Snippet.objects.all()
-#     serializer_class = SnippetSerializer
-
-
-#     def get(self, request, *args, **kwargs):
-#         return self.retrieve(request, *args, **kwargs)
-
-#     def put(self, request, *args, **kwargs):
-#         return self.update(request, *args, **kwargs)
-
-#     def delete(self, request, *args, **kwargs):
-#         return self.destroy(request, *args, **kwargs)
-
-# class UserList(generics.ListAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
-#     def get(self, request, *args, **kwargs):
-#         return self.list(request, *args, **kwargs)
-
-#     def post(self, request, *args, **kwargs):
-#         return self.create(request, *args, **kwargs)
-    
-# class UserDetail(generics.RetrieveAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-    
-#     def get(self, request, *args, **kwargs):
-#         return self.retrieve(request, *args, **kwargs)
-
-#     def put(self, request, *args, **kwargs):
-#         return self.update(request, *args, **kwargs)
-
-#     def delete(self, request, *args, **kwargs):
-#         return self.destroy(request, *args, **kwargs)
 
 class SnippetViewSet(viewsets.ModelViewSet):
     queryset =Snippet.objects.all()
